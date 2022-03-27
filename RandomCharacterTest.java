12f2fe13e0d16bd58b123bfc1cc9e79a9de8470e
DayceeLau <97490603+DayceeLau@users.noreply.github.com>
2022-03-27 23:23:14 +0800

    Update RandomCharacterTest.java

--- RandomCharacterTest.java
+++ RandomCharacterTest.java
@@ -3,44 +3,44 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.*;
 
-public class Wk09RandomCharacterTest {
-    Wk09RandomCharacter randomCharacter;
+public class RandomCharacterTest {
+    RandomCharacter randomChar;
     @BeforeEach
     @Test
     void setup(){
-        randomCharacter = new Wk09RandomCharacter();
-        assertTrue(randomCharacter instanceof Wk09RandomCharacter);
+        randomChar = new RandomCharacter();
+        assertTrue(randomChar instanceof RandomCharacter);
     }
 
     @RepeatedTest(15)
     void getRandomLowerCaseLetterTest(){
-        assertTrue(Character.isLowerCase(randomCharacter.getRandomLowerCaseLetter()));
-        assertFalse(Character.isUpperCase(randomCharacter.getRandomLowerCaseLetter()));
+        assertTrue(Character.isLowerCase(randomChar.getRandomLowerCaseLetter()));
+        assertFalse(Character.isUpperCase(randomChar.getRandomLowerCaseLetter()));
     }
 
     @RepeatedTest(15)
     void getRandomUpperCaseLetterTest(){
-        assertTrue(Character.isUpperCase(randomCharacter.getRandomUpperCaseLetter()));
-        assertFalse(Character.isLowerCase(randomCharacter.getRandomUpperCaseLetter()));
+        assertTrue(Character.isUpperCase(randomChar.getRandomUpperCaseLetter()));
+        assertFalse(Character.isLowerCase(randomChar.getRandomUpperCaseLetter()));
     }
 
     @RepeatedTest(15)
     void getRandomDigitCharacterTest(){
-        assertTrue(Character.isDigit(randomCharacter.getRandomDigitCharacter()));
-        assertFalse(Character.isUpperCase(randomCharacter.getRandomDigitCharacter()));
-        assertFalse(Character.isLowerCase(randomCharacter.getRandomDigitCharacter()));
-        assertFalse(Character.isLetter(randomCharacter.getRandomDigitCharacter()));
+        assertTrue(Character.isDigit(randomChar.getRandomDigitCharacter()));
+        assertFalse(Character.isUpperCase(randomChar.getRandomDigitCharacter()));
+        assertFalse(Character.isLowerCase(randomChar.getRandomDigitCharacter()));
+        assertFalse(Character.isLetter(randomChar.getRandomDigitCharacter()));
     }
 
     @RepeatedTest(15)
     void getRandomCharacterTest(){
-        assertTrue(randomCharacter.getRandomUpperCaseLetter() <=256);
-        assertFalse(randomCharacter.getRandomUpperCaseLetter() >256);
+        assertTrue(randomChar.getRandomUpperCaseLetter() <=256);
+        assertFalse(randomChar.getRandomUpperCaseLetter() >256);
     }
 
     @RepeatedTest(15)
     void getRandomPrimeNumberTest(){
-        int number = randomCharacter.getRandomPrimeNumber();
-        assertTrue(randomCharacter.Prime(number));
+        int number = randomChar.getRandomPrimeNumber();
+        assertTrue(randomChar.Prime(number));
     }
 }
