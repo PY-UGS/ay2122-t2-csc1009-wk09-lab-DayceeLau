b9e770f006c44f52feeeb7620e68d8f1a01d1d04
DayceeLau <97490603+DayceeLau@users.noreply.github.com>
2022-03-27 23:30:37 +0800

    Update RandomCharacterTest.java

--- RandomCharacterTest.java
+++ RandomCharacterTest.java
@@ -8,23 +8,26 @@ public class RandomCharacterTest {
     @BeforeEach
     @Test
     void setup(){
-        randomChar = new RandomCharacter();
-        assertTrue(randomChar instanceof RandomCharacter);
+        randomChar = new RandomCharacter();     //Create RandomCharacter object
+        assertTrue(randomChar instanceof RandomCharacter);      //Check if object is instance of RandomCharacter
     }
 
     @RepeatedTest(15)
+    //Check if lowercase
     void getRandomLowerCaseLetterTest(){
-        assertTrue(Character.isLowerCase(randomChar.getRandomLowerCaseLetter()));
-        assertFalse(Character.isUpperCase(randomChar.getRandomLowerCaseLetter()));
+        assertTrue(Character.isLowerCase(randomChar.getRandomLowerCaseLetter()));   
+        assertFalse(Character.isUpperCase(randomChar.getRandomLowerCaseLetter()));  
     }
 
     @RepeatedTest(15)
+    //Check if upppercase
     void getRandomUpperCaseLetterTest(){
         assertTrue(Character.isUpperCase(randomChar.getRandomUpperCaseLetter()));
         assertFalse(Character.isLowerCase(randomChar.getRandomUpperCaseLetter()));
     }
 
     @RepeatedTest(15)
+    //Check digit character
     void getRandomDigitCharacterTest(){
         assertTrue(Character.isDigit(randomChar.getRandomDigitCharacter()));
         assertFalse(Character.isUpperCase(randomChar.getRandomDigitCharacter()));
@@ -33,12 +36,14 @@ public class RandomCharacterTest {
     }
 
     @RepeatedTest(15)
+    //Check number of random character
     void getRandomCharacterTest(){
         assertTrue(randomChar.getRandomUpperCaseLetter() <=256);
         assertFalse(randomChar.getRandomUpperCaseLetter() >256);
     }
 
     @RepeatedTest(15)
+    //Check for prime number
     void getRandomPrimeNumberTest(){
         int number = randomChar.getRandomPrimeNumber();
         assertTrue(randomChar.Prime(number));
